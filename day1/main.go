d4403ba8cdac65889ddac3e8029cc6b6aeb6e21a
orn <orna91@gmail.com>
2022-12-10 16:32:28 +0000

    day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -17,23 +17,44 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	var currentCal, mostCal, calLine int
+	var currentCal, calLine int
+
+	ok := false
+	top := [3]int{}
+
 	for scanner.Scan() {
 		calLine, _ = strconv.Atoi(scanner.Text())
-		mostCal, currentCal = process(mostCal, currentCal, calLine)
+		currentCal, ok = process(currentCal, calLine)
+
+		if ok {
+			top = checkIfMore(top, currentCal)
+			currentCal = 0
+		}
 	}
 
-	fmt.Println("most calories", mostCal)
+	fmt.Println("most calories: %d", top[0]+top[1]+top[2])
 }
 
-func process(most, current, calories int) (int, int) {
+func process(total, calories int) (int, bool) {
 	if calories == 0 {
-		return most, 0
+		return total, true
 	}
 
-	if current += calories; current > most {
-		most = current
+	return total + calories, false
+}
+
+func checkIfMore(top [3]int, current int) [3]int {
+	if current <= top[2] { // Not top 3
+		return top
 	}
 
-	return most, current
+	if current <= top[1] { // Top 3
+		return [3]int{top[0], top[1], current}
+	}
+
+	if current <= top[0] { // Top 2
+		return [3]int{top[0], current, top[1]}
+	}
+
+	return [3]int{current, top[0], top[1]} // Top 1
 }
